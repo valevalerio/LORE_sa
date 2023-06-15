a4836924044c43c547e49a2c3159d566107210c2
mesosbrodleto <paolo.cintia@gmail.com>
2023-06-15 13:13:34 +0200

    Target encoder and decoder stable

--- lore_sa/encoder_decoder/my_target_enc.py
+++ lore_sa/encoder_decoder/my_target_enc.py
@@ -3,20 +3,19 @@ from category_encoders import TargetEncoder
 from lore_sa.dataset.dataset import Dataset
 import pandas as pd
 
-__all__ = ["EncDec", "MyTargetEnc"]
+__all__ = ["EncDec", "TargetEnc"]
 
 extend: TargetEncoder
-class MyTargetEnc(EncDec):
+class TargetEnc(EncDec):
     """
     Target encoding for categorical features.
     Extend TargetEncoder from category_encoders.
     """
     def __init__(self):
         super().__init__()
-        self.type = "target"
 
 
-    def encode(self, dataset: Dataset, features_to_encode: list, target: str):
+    def encode(self, dataset: Dataset, features_to_encode: list, target):
         """
         It applies the encoder to the input categorical features
 
@@ -24,13 +23,15 @@ class MyTargetEnc(EncDec):
         :param[list] features_to_encode: list of columns of Dataset.df dataframe to be encoded
         :param[str] target: target name column
         """
+        
         y = dataset.df[target].values
-        self.dataset_encoded = TargetEncoder(return_df=False).fit_transform(dataset.df,y)
+        self.dataset_encoded = TargetEncoder(return_df=True, cols = features_to_encode).fit_transform(dataset.df,y)
 
         # buffering original features to get them eventually decoded
-        self.original_data = dataset.df[features_to_encode]
+        self.original_data = dataset.df
         self.original_features = features_to_encode
         self.encoded_features = self.dataset_encoded.columns
+        
 
         # substituing old features with encoded
         dataset.df = pd.concat([dataset.df.drop(columns=features_to_encode), self.dataset_encoded], axis=1)
@@ -39,12 +40,17 @@ class MyTargetEnc(EncDec):
 
     def __str__(self):
         if self.encoded_features is not None:
-            return ("TargetEncoder - features encoded: %s"%(",".join(self.original_features)))
+            return ("TargetEncoder - original features encoded: %s"%(",".join(self.original_features)))
         else:
             return ("TargetEncoder - no features encoded")
 
     def decode(self, dataset: Dataset, kwargs=None):
         if self.encoded_features is not None:
-            return pd.concat([dataset.df.drop(columns =self.encoded_features), self.original_data] , axis =1)
+            decoded_data = self.original_data
+            self.dataset_encoded = None
+            self.original_features = None
+            self.original_data = None
+            self.encoded_features = None        
+            return decoded_data
         else:
             return dataset.df
