aae5b0cc942533a9e956a63230113e4e37856ecc
mesosbrodleto <paolo.cintia@gmail.com>
2023-06-22 17:25:44 +0200

    one hot encoder edited - returning numpy array, both encoded and decoded

--- lore_sa/encoder_decoder/one_hot_enc.py
+++ lore_sa/encoder_decoder/one_hot_enc.py
@@ -1,5 +1,6 @@
 from .enc_dec import EncDec
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 from lore_sa.dataset.dataset import Dataset
 
@@ -9,30 +10,39 @@ __all__ = ["EncDec", "OneHotEnc"]
 class OneHotEnc(EncDec):
     """
     It provides an interface to access One Hot enconding (https://en.wikipedia.org/wiki/One-hot) functions. 
+    It relies on OneHotEncoder class from sklearn
     """
 
     def __init__(self):
         super().__init__()
-        self.type = "one-hot"
+        self.encoder = OneHotEncoder()
 
     def encode(self, dataset: Dataset, features_to_encode: list):
         """
-        It applies the encoder to the input features
+        It applies the encoder to the input features. It also modifies the input dataset object, adding the
+        encoded version of the dataset
 
         :param[Dataset] x: the Dataset containing the features to be encoded
         :param[list] features_to_encode: list of columns of Dataset.df dataframe to be encoded
         """
-        self.dataset_encoded = pd.get_dummies(dataset.df[features_to_encode], prefix_sep='=')
+        
+        #tranforming everything to numpy, in order to not get everything messed by pandas indexing
+        encoded = self.encoder.fit_transform(dataset.df[features_to_encode].to_numpy()).toarray()
+        original = dataset.df.drop(columns=features_to_encode)
+        self.original_features_skipped = original.columns
+        original = original.to_numpy()
 
-        # buffering original features to get them eventually decoded
+        dataset.dataset_encoded = pd.DataFrame(encoded, columns = self.encoder.get_feature_names_out(dataset.df[features_to_encode].columns))
+        # buffering original features
         self.original_data = dataset.df[features_to_encode]
-        self.original_features = features_to_encode
-        self.encoded_features = self.dataset_encoded.columns
+   
+        self.original_features_encoded = features_to_encode
+        self.encoded_features = dataset.dataset_encoded.columns
 
         # substituing old features with encoded
-        dataset.df = pd.concat([dataset.df.drop(columns=features_to_encode), self.dataset_encoded], axis=1)
+        dataset.df = pd.concat([pd.DataFrame(original, columns = dataset.df.drop(columns=features_to_encode).columns), dataset.dataset_encoded], axis=1)
 
-        return dataset.df
+        return dataset.df.to_numpy()
 
     def __str__(self):
         if self.encoded_features is not None:
@@ -40,12 +50,26 @@ class OneHotEnc(EncDec):
         else:
             return "OneHotEncoder - no features encoded"
 
-    def decode(self, dataset: Dataset, kwargs=None):
+    def decode(self, dataset: Dataset):
+        """
+        It decodes the input (encoded) dataset using onehotencoder inverse_transform. 
+        
 
-        if self.encoded_features is not None:
-            if self.original_data is None:
-                raise Exception("ERROR! To decode a dataset it must be firstly encoded by the same encoder object.")
+        :param 
+        """
 
-            return pd.concat([dataset.df.drop(columns=self.encoded_features), self.original_data], axis=1)
+        if dataset.dataset_encoded is None:
+            raise Exception("No encoded dataset found")
+        
         else:
-            return dataset.df
+            #tranforming everything to numpy, in order to not get everything messed by pandas indexing
+
+            decoded = self.encoder.inverse_transform(dataset.dataset_encoded.to_numpy())
+            original = dataset.df.drop(columns=self.encoded_features).to_numpy()
+            
+                
+            dataset.df = pd.concat([pd.DataFrame(original, columns = self.original_features_skipped),
+                                     pd.DataFrame(decoded, columns = self.original_features_encoded)], axis=1)
+
+            
+            return dataset.df.to_numpy()
