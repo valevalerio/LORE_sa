68bac8b02d93e01ec9c90311d3346b00dbc0148a
Andrea Spinelli <spinelli87@gmail.com>
2023-06-27 11:24:38 +0200

    add surrogate class

--- test/test_surrogate.py
+++ test/test_surrogate.py
@@ -1,7 +1,16 @@
 import unittest
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+from lore_sa.surrogate import DecTree
 
 class SurrogateTest(unittest.TestCase):
-    pass
+
+    def test_init(self):
+
+        iris = load_iris()
+        dt = DecTree().train(iris.data, iris.target)
+        self.assertIsInstance(dt, DecisionTreeClassifier)
 
 
 if __name__ == '__main__':
