498d66198b62ffd52148a17ed7d7ff531b8c1a92
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2024-06-05 10:34:30 +0200

    fixing generation of documentation

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..')))
+# sys.path.insert(0, os.path.abspath(os.path.join('..')))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 print('sys.path', sys.path)
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
